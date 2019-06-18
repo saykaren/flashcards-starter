e14e184326e26ff8467a338b9468ed9f7bb399ac
Karen Garcia <karen.garcia@nexscreen.com>
2019-06-18 12:36:42 -0600

    All classes except for Round are working. Working on getting the super from Turn to go into Round

--- src/Card.js
+++ src/Card.js
@@ -43,29 +43,6 @@ class Turn{
   
 }
 
-const card = new Card(1, "What is Karen\'s favorite investment", ["single stock", "ETF", "Bonds", "Realestate", "under my mattress fool"], "ETF");
-const turn = new Turn("ETF", card);
-const turn2 = new Turn("ETF", card);
-
-
-turn.returnGuess()
-turn.returnCard()
-turn.evaluateGuess()
-turn.giveFeedback()
-
-function sayIt(ar1){
-  console.log(ar1);
-};
-
-function sayItAgain(turnNum){
-  sayIt(turnNum.returnGuess());
-  sayIt(turnNum.returnCard());
-  sayIt(turnNum.evaluateGuess());
-  sayIt(turnNum.giveFeedback());
-}
-
-// sayItAgain(turn);
-// sayItAgain(turn2);
 
 ////////////////Deck Class
 class Deck{
@@ -93,23 +70,13 @@ class Deck{
 }
 
 
-const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-const card4 = new Card(10, 'What is George\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Lex');
-
-const deck = new Deck([card2, card1, card3, card4]);
-
-// console.log("Deck count "+deck.countCards());
-// console.log("cardDetails are "+ deck.cardDetails());
-
-var detailsCard = deck.cardDetails();
 
 
 ///////////////Round Class//
 //takes and records guess also compares to evaluateGuess() from Turn class
-class Round {
-  constructor(deck){
+class Round extends Turn{
+  constructor(deck, userAnswer, card){
+    super(userAnswer, card);
     this.deck = deck;
     this.incorrectguesses = [];
   }
@@ -120,12 +87,29 @@ class Round {
     return detailsCard[0];
   }
   
+  //takeTuren not working.....6.18.2019 ............10:31am
   takeTurn(){
-    console.log("here at takeTurn");
     /////////boolean if guess was right
+    //const newTurn = new Turn(Turn.returnGuess(), card);
     
-    console.log(this.incorrectguesses);
-    console.log(turn.evaluateGuess());
+    // const newTurn = new Turn(super.userAnswer, super.card);
+    // console.log(super.userAnswer);
+    // console.log(super.card);
+    // console.log(newTurn);
+
+    // console.log(newTurn);
+    // console.log(turn);
+    // console.log(super.evaluateGuess());
+    // console.log(this.returnCurrentCard());
+    // var result = 
+    // console.log(this.incorrectguesses);
+    // if (turn.evaluateGuess() !== true){
+    //   this.incorrectguesses.push(deck.cardDetails());
+    // }
+    // console.log(turn.evaluateGuess());
+    // return newTurn;
+    // return result;
+    // return newTurn;
   }
 
   calculatePercentCorrect(){
@@ -133,10 +117,51 @@ class Round {
   }
 
   endRound(){
-    //method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
+    //method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!
+    console.log(`**Round Over!** You answered ${calculatePercentCorrect()}% of the questions correctly!`);
   }
 
 }
+
+
+const card = new Card(1, "What is Karen\'s favorite investment", ["single stock", "ETF", "Bonds", "Realestate", "under my mattress fool"], "ETF");
+const turn = new Turn("Bonds", card);
+const turn2 = new Turn("single stock", card);
+const turn3 = new Turn("ETF", card);
+
+
+turn.returnGuess()
+turn.returnCard()
+turn.evaluateGuess()
+turn.giveFeedback()
+
+function sayIt(ar1){
+  console.log(ar1);
+};
+
+function sayItAgain(turnNum){
+  sayIt(turnNum.returnGuess());
+  sayIt(turnNum.returnCard());
+  sayIt(turnNum.evaluateGuess());
+  sayIt(turnNum.giveFeedback());
+}
+
+sayItAgain(turn);
+sayItAgain(turn2);
+sayItAgain(turn3);
+
+
+const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+const card4 = new Card(10, 'What is George\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Lex');
+
+const deck = new Deck([card, card1, card2, card1, card3, card4]);
+
+console.log("Deck count "+deck.countCards());
+// console.log("cardDetails are "+ deck.cardDetails());
+
+var detailsCard = deck.cardDetails();
 
 const round = new Round(deck);
 const firstCard = round.returnCurrentCard();
