2baac67d952837a56ac2d8662a9c5ff051b27beb
Karen Garcia <karen.garcia@nexscreen.com>
2019-06-20 10:44:54 -0600

    cleaned up code added configuration for Round.calculatePercentCorrect() and Round.endRound()

--- src/Card.js
+++ src/Card.js
@@ -35,12 +35,9 @@ class Turn{
     if(this.userAnswer == this.card.correctAnswer){
       return "correct!";
     } else{
-      // console.log(`incorrect as.... ${this.evaluateGuess()}`);
-      // console.log(typeof this.evaluateGuess());
       return "incorrect!";
     }
   }
-  
 }
 
 
@@ -63,32 +60,20 @@ class Deck{
         acc.push(card) 
         return acc
     }, []);
-    // console.log(newArray);
     return newArray;
   }
 
 }
 
 
-
-
 ///////////////Round Class//
-//takes and records guess also compares to evaluateGuess() from Turn class
-
-// class Round extends Turn{
-//   constructor(deck, userAnswer, card){
-//     super(userAnswer, card);
-//     this.deck = deck;
-//     this.incorrectguesses = [];
-//   }
 class Round {
   constructor(deck){
     this.deck = deck;
     this.incorrectguesses = 0;
     this.turnCount = 0;
     this.filteredDeck = this.deck;
-  }
-    
+  }    
 
   returnCurrentCard(){
     ///////method that returns the current card being played
@@ -96,13 +81,7 @@ class Round {
     return detailsCard[0];
   }
   
-  //takeTuren not working.....
-  takeTurn(userGuess){
-    //pseudo code new Turn instance is created with input of new guess. 
-       //need to get card details from Deck and add one to Deck
-
-    ///set a new variable 
-    
+  takeTurn(userGuess){ 
     const newTurn = new Turn(userGuess, card);
     const results = newTurn.evaluateGuess();
     this.turnCount++;
@@ -110,28 +89,26 @@ class Round {
       this.incorrectguesses++;
       return "Incorrect";
     } else {
-      // this.filteredDeck = this.deck.filter(currCard => currCard.num !== card.num);
-      console.log(this.deck);
       this.filteredDeck = this.deck.cardNum.filter(currCard => currCard.num !== card.num);
       return "correct!";
     };
-  
   }
 
   calculatePercentCorrect(){
-//method that calculates and returns the percentage of correct guesses
+    const percentRight = ((this.turnCount - this.incorrectguesses)/this.turnCount)*100;
+    return percentRight;
   }
 
   endRound(){
     //method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!
-    console.log(`**Round Over!** You answered ${calculatePercentCorrect()}% of the questions correctly!`);
+    const result = round1.calculatePercentCorrect();
+    console.log(`**Round Over!** You answered ${result}% of the questions correctly!`);    
   }
 
 }
 
 /////////Cards -Creating new Cards with details
-const card = new Card(1, "What is Karen\'s favorite investment", ["single stock", "ETF", "Bonds", "Realestate", "under my mattress fool"], "ETF");
-console.log(card);
+const card = new Card(20, "What is Karen\'s favorite investment", ["single stock", "ETF", "Bonds", "Realestate", "under my mattress fool"], "ETF");
 const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -143,24 +120,6 @@ const turn = new Turn("Bonds", card);
 const turn2 = new Turn("single stock", card);
 const turn3 = new Turn("ETF", card);
 
-///Function to output into Console Turn details 
-function sayIt(ar1){
-  console.log(ar1);
-};
-
-function sayItAgain(turnNum){
-  sayIt(turnNum.returnGuess());
-  sayIt(turnNum.returnCard());
-  sayIt(turnNum.evaluateGuess());
-  sayIt(turnNum.giveFeedback());
-}
-
-// To check turns details 
-// sayItAgain(turn);
-// sayItAgain(turn2);
-// sayItAgain(turn3);
-
-
 
 //////////Deck - to issues a new Deck with certain cards - right now all the cards I made
 const deck = new Deck([card, card1, card2, card1, card3, card4]);
@@ -170,27 +129,7 @@ const round1 = new Round(deck);
 
 round1.takeTurn("stock");
 round1.takeTurn("bonds");
+round1.takeTurn("under my mattress fool");
+round1.takeTurn("Realestate");
 round1.takeTurn("ETF");
-round1.takeTurn("stocks");
-round1.takeTurn("bonds");
-console.log(round1.filteredDeck);
 
-console.log(`Hello ${round1Result}`);
-console.log(round1Result);
-
-//Checking how many cards are in Deck - working 06/18 ...12:44pm
-// console.log("Deck count "+deck.countCards());
-
-//Checking if card details are coming out --working 06/18 @ 12:45pm
-// var detailsCard = deck.cardDetails();
-// console.log(detailsCard);
-
-//Checking if first card is working - Yes it is 06/18/2019 @ 12:46pm 
-// const firstCard = round1.returnCurrentCard();
-// console.log(firstCard);
-
-
-// console.log(firstCard);
-// console.log("The round has the first card, did it work? " + firstCard); 
-// console.log(deck);
-// console.log(round.takeTurn());
